cb3e2a75d10c349457c455ac385021453abc5d16
ElMahdi-H <195800184+ElMahdi-H@users.noreply.github.com>
2026-05-22 10:55:40 +0200

    Made Player cpp and h files

---
+++ src/Player.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Elmahdi on 22/05/2026.
+//
+
+#include "Player.h"
+#include <iostream>
+
+void Player::TakeDamage(int Damage) {
+    health -= Damage;
+
+    std::cout << "Player took Damage" << std::endl;
+}
+
+void PlayerMovement() {
+    if () {
+
+    }
+}
+
---
+++ src/Player.h
@@ -0,0 +1,16 @@
+//
+// Created by Elmahdi on 22/05/2026.
+//
+
+#ifndef GAME_JAM_WIRWARRUNNER_PLAYER_H
+#define GAME_JAM_WIRWARRUNNER_PLAYER_H
+
+#endif //GAME_JAM_WIRWARRUNNER_PLAYER_H
+
+class Player {
+public:
+    int health = 3;
+
+    void TakeDamage(int Damage);
+};
+
